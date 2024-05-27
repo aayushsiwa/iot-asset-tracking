06da6fbd967ca116848ee379b5f2e863d33f2f15
Aayush Siwa <22052177@kiit.ac.in>
2024-05-27 19:47:51 +0530

    connected supabase db

--- app/page.tsx
+++ app/page.tsx
@@ -1,113 +1,127 @@
-import Image from "next/image";
+"use client";
+import { useEffect, useState } from "react";
+import { createClient } from "@supabase/supabase-js";
 
-export default function Home() {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:size-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+const supUrl: string = process.env.NEXT_PUBLIC_SUPABASE_URL || "";
+const anonKey: string = process.env.NEXT_PUBLIC_SUPABASE_KEY || "";
+const supabase = createClient(supUrl, anonKey);
+
+const Home = () => {
+    const [assets, setAssets] = useState<any[]>([]);
+    const [name, setName] = useState<string>("");
+    const [status, setStatus] = useState<string>("");
+    const [location, setLocation] = useState<string>("");
+
+    useEffect(() => {
+        fetchAssets();
+    }, []);
+
+    const fetchAssets = async () => {
+        try {
+            const { data, error } = await supabase.from("assets").select();
+            if (error) {
+                throw error;
+            }
+            setAssets(data || []);
+        } catch (error) {
+            console.error("Error fetching assets:", error.message);
+        }
+    };
+
+    const handleAddAsset = async () => {
+        try {
+            let locationToSet = location; // Default location
+
+            // Set location based on selected status
+            switch (status) {
+                case "Maintenance":
+                    locationToSet = "Workshop";
+                    break;
+                case "Available":
+                    locationToSet = "Store Room";
+                    break;
+                default:
+                    break;
+            }
+
+            const { error } = await supabase.from("assets").insert([
+                {
+                    name,
+                    status,
+                    location: locationToSet,
+                    lastUpdated: new Date().toISOString(),
+                },
+            ]);
+            if (error) {
+                throw error;
+            }
+            fetchAssets();
+        } catch (error) {
+            console.error("Error adding asset:", error.message);
+        }
+    };
+
+    return (
+        <div className="container flex flex-col mx-auto gap-y-10">
+            <h1 className="text-3xl font-bold mt-8 mb-4">
+                Asset Tracking Dashboard
+            </h1>
+            <h2 className="text-xl font-semibold">Add Asset</h2>
+            <div className="flex gap-2">
+                <input
+                    type="text"
+                    className="border p-2 mb-2 bg-black"
+                    placeholder="Name"
+                    value={name}
+                    onChange={(e) => setName(e.target.value)}
+                />
+                <select
+                    className="border p-2 mb-2 bg-black text-gray-400"
+                    value={status}
+                    onChange={(e) => setStatus(e.target.value)}
+                >
+                    <option value="">Select Status</option>
+                    <option value="In Use">In Use</option>
+                    <option value="Available">Available</option>
+                    <option value="Maintenance">Maintenance</option>
+                </select>
+                <select
+                    className="border p-2 mb-2 bg-black text-gray-400"
+                    value={location}
+                    onChange={(e) => setLocation(e.target.value)}
+                    disabled={
+                        status === "Maintenance" || status === "Available"
+                    }
+                >
+                    <option value="">
+                        {status === "Maintenance"
+                            ? "Workshop"
+                            : status === "Available"
+                            ? "Store Room"
+                            : "Select Location"}
+                    </option>
+                    <option value="Main Office">Main Office</option>
+                    <option value="Storage Room">Storage Room</option>
+                    <option value="Maintenance">Maintenance</option>
+                </select>
+                <button
+                    className="bg-black border-2 text-white px-4 py-2 rounded"
+                    onClick={handleAddAsset}
+                >
+                    Add Asset
+                </button>
+            </div>
+            <div className="grid grid-cols-3 gap-4">
+                {assets.map((asset, index) => (
+                    <div key={index} className="border p-4 rounded-md">
+                        <h2 className="text-xl font-semibold">{asset.name}</h2>
+                        <p>Status: {asset.status}</p>
+                        <p>Location: {asset.location}</p>
+                    </div>
+                ))}
+            </div>
         </div>
-      </div>
+    );
+};
 
-      <div className="relative z-[-1] flex place-items-center before:absolute before:h-[300px] before:w-full before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 sm:before:w-[480px] sm:after:w-[240px] before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-balance text-sm opacity-50">
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  );
-}
+export default Home;
