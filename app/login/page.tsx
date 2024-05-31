7f1d404978f716010108ef0b7438d179532ac7ef
Aayush Siwa <22052177@kiit.ac.in>
2024-05-31 12:52:49 +0530

    vercel up

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,45 +1,45 @@
-// "use client";
-// import React from "react";
-// import { supabase } from "../lib/helper/supabaseClient";
-// import { Auth } from "@supabase/auth-ui-react";
-// import { ThemeSupa } from "@supabase/auth-ui-shared";
+"use client";
+import React from "react";
+import { supabase } from "../lib/helper/supabaseClient";
+import { Auth } from "@supabase/auth-ui-react";
+import { ThemeSupa } from "@supabase/auth-ui-shared";
 
-// function page() {
-//     const loginGitHub = async () => {
-//         const { data, error } = await supabase.auth.signInWithOAuth({
-//             provider:"github",
-//             options: {
-//                 redirectTo: `../auth/callback`,
-//             },
-//         });
-//     };
-//     // async function signOut() {
-//     //     const { error } = await supabase.auth.signOut();
-//     // }
+function page() {
+    const loginGitHub = async () => {
+        const { data, error } = await supabase.auth.signInWithOAuth({
+            provider:"github",
+            options: {
+                redirectTo: `../auth/callback`,
+            },
+        });
+    };
+    // async function signOut() {
+    //     const { error } = await supabase.auth.signOut();
+    // }
 
-//     return (
-//         <>
-//             <button className="border-2 hover:bg-zinc-700" onClick={loginGitHub}>
-//                 Login with github
-//             </button>
-//             {/* <Auth
-//                 supabaseClient={supabase}
-//                 appearance={{ theme: ThemeSupa }}
-//                 providers={["github"]}
-//                 localization={{
-//                     variables: {
-//                         sign_in: {
-//                             email_label: "Your email address",
-//                             password_label: "Your strong password",
-//                         },
-//                         sign_up: {
-//                             email_label: "Your email address",
-//                         },
-//                     },
-//                 }}
-//             /> */}
-//         </>
-//     );
-// }
+    return (
+        <>
+            <button className="border-2 hover:bg-zinc-700" onClick={loginGitHub}>
+                Login with github
+            </button>
+            {/* <Auth
+                supabaseClient={supabase}
+                appearance={{ theme: ThemeSupa }}
+                providers={["github"]}
+                localization={{
+                    variables: {
+                        sign_in: {
+                            email_label: "Your email address",
+                            password_label: "Your strong password",
+                        },
+                        sign_up: {
+                            email_label: "Your email address",
+                        },
+                    },
+                }}
+            /> */}
+        </>
+    );
+}
 
-// export default page;
+export default page;
